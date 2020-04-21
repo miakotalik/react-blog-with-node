cfd2d38c081535e5b4f10ee0f8eb47a62ebcc8b8
miakotalik <mia@rmtwentynine.com>
2020-04-21 17:39:21 -0400

    added get and post req to node server

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,5 +4,8 @@ const app = express();
 
 
 app.get('/hello', (req, res) => res.send("Hello"));
+app.post("/hello", (req, res) => res.send("Hello"));
+
+
 
 app.listen(8000, () => console.log("Listening on port 800"));
