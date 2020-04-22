34f069943332b8df54900e9b03821fe0efd1e1cc
miakotalik <mia@rmtwentynine.com>
2020-04-21 22:18:06 -0400

    added get request with mongodb connection

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
 
 //  "fake" database created inside a constant. Used in development to test upvote and comment functionalities
@@ -19,12 +20,26 @@ const articlesInfo = {
     },
 }
 
-
-
-
-
 const app = express();
 app.use(bodyParser.json());
+
+// get request to get articles from MongoDb database
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
+
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+
+})
 
 // post request that upvotes the matching article by 1
 app.post('/api/articles/:name/upvote', (req, res) => {
