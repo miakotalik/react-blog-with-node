d96c537b3405ee9e8ada143860bd0d46d5f4bb8d
miakotalik <mia@rmtwentynine.com>
2020-04-21 23:36:31 -0400

    updated comments and upvote endpoints to work with mongodb

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -2,52 +2,39 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 
-
-//  "fake" database created inside a constant. Used in development to test upvote and comment functionalities
-// use postman to test requests and responses
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: [],
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: [],
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: [],
-    },
-}
-
 const app = express();
+
 app.use(bodyParser.json());
 
-// get request to get articles from MongoDb database
-app.get('/api/articles/:name', async (req, res) => {
+// connect to database applies operations and closes connection
+const withDB = async (operations, res) => {
     try {
-        const articleName = req.params.name;
-
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('my-blog');
 
-        const articleInfo = await db.collection('articles').findOne({ name: articleName })
-        res.status(200).json(articleInfo);
+        await operations(db);
 
         client.close();
     } catch (error) {
         res.status(500).json({ message: 'Error connecting to db', error });
     }
+}
 
-})
-
-// post request that upvotes the matching article by 1
-app.post('/api/articles/:name/upvote', async (req, res) => {
-    try {
+// get article based on matching name in the database
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
         const articleName = req.params.name;
 
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('my-blog');
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+    }, res);
+})
+
+
+// upvotes article with matching name in the database
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
 
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({ name: articleName }, {
@@ -58,23 +45,26 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
 
         res.status(200).json(updatedArticleInfo);
-
-        client.close();
-    } catch (error) {
-        res.status(500).json({ message: 'Error connecting to db', error });
-    }
+    }, res);
 });
+
 
 // post request that adds given comment to the matching article
 app.post('/api/articles/:name/add-comment', (req, res) => {
     const { username, text } = req.body;
     const articleName = req.params.name;
 
-    articlesInfo[articleName].comments.push({ username, text });
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                comments: articleInfo.comments.concat({ username, text }),
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
 
-    res.status(200).send(articlesInfo[articleName]);
-
-
+        res.status(200).json(updatedArticleInfo);
+    }, res);
 });
 
 // shows us the app is listening
